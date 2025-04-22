3c0072809f67f32ee11c373014d9bb1e1232fc92
JesterAV <winchesrers@gmail.com>
2025-04-22 10:53:40 +0300

    feat: create picture explore section

--- components/picture explore/picture_explore.mjs
+++ components/picture explore/picture_explore.mjs
@@ -4,8 +4,18 @@ export function createPictureExplore() {
     const pictureExploreSection = createElement('div', 'pictureExploreSection', document.body);
     const pictureExploreContainer = createElement('div', 'pictureExploreContainer', pictureExploreSection);
     const pictureExploreTextContainer = createElement('div', 'pictureExploreText', pictureExploreContainer);
-    const pictureExploreImageContainer = createElement('div', 'pictureExploreImage', pictureExploreContainer);
+    const pictureExploreImageContainer = createElement('div', 'pictureExploreImageContainer', pictureExploreContainer);
     const pictureExploreTitleContainer = createElement('div', 'pictureExploreTitleContainer', pictureExploreTextContainer);
     const pictureExploreTitle = createElement('span', 'pictureExploreTitle', pictureExploreTitleContainer);
-    const pictureExploreTextParagraphOne = createElement ('p', 'pictureExploreParagraph');
+    const pictureExploreTextParagraphOne = createElement ('p', 'pictureExploreParagraph', pictureExploreTextContainer);
+    const pictureExploreTextParagraphTwo = createElement ('p', 'pictureExploreParagraph', pictureExploreTextContainer);
+    const pictureExploreTextParagraphThree = createElement ('p', 'pictureExploreParagraph', pictureExploreTextContainer);
+    const pictureExploreImage = createElement('img', 'pictureExploreImage', pictureExploreImageContainer);
+
+    pictureExploreImage.src = '/museum/assets/images/png/picture-explore.png';
+    pictureExploreTitle.textContent = 'Picture explore';
+    pictureExploreTextParagraphOne.textContent = 'Las Meninas is a 1656 painting by Diego Velázquez, the leading artist of the Spanish Golden Age.';
+    pictureExploreTextParagraphTwo.textContent = 'It was cleaned in 1984 to remove a "yellow veil" of dust that had gathered since the previous restoration in the 19th century.';
+    pictureExploreTextParagraphThree.textContent = 'The cleaning provoked furious protests, not because the picture had been damaged in any way, but because it looked different.';
+
 }
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { createHeader } from "./components/header/header.mjs";
 import { createMain } from "./components/main/main.mjs";
 import { createVirtualTourSection } from "./components/virtual tour/virtual_tour.mjs";
+import { createPictureExplore } from "./components/picture explore/picture_explore.mjs";
 
 createMain();
 createVirtualTourSection();
+createPictureExplore();
